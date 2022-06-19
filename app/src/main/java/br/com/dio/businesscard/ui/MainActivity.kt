d07ac14be4ddacd6c7363865a33a0d21be06febf
volnei <volnei.filho@best2bee.com.br>
2022-06-19 03:10:01 -0300

    First commit

---
+++ app/src/main/java/br/com/dio/businesscard/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package br.com.dio.businesscard.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.dio.businesscard.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
